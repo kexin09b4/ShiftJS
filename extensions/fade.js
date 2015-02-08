fed316d44121be221930e26c35e555b32056a37d
Dan Zervoudakes <dan.zervoudakes@gmail.com>
2015-02-08 15:51:12 -0500

    Finalized 'fadeIn'.

--- extensions/fade.js
+++ extensions/fade.js
@@ -4,14 +4,14 @@
  	
 	shift.fn.fadeOut = function(_duration){
 		
-		var i, j, timer, easing, callback, collection;
+		var i, j, timer, callback, collection;
 		
 		collection = this.set;
 		timer = (_duration && typeof _duration === "number") ? _duration + "s" : "0.5s";
 		
 		for (i = 0; i < collection.length; i++){
-					
-			collection[i].style.transition = "all " + timer/* + easing*/; // NEED TO INCLUDE EASING HERE SOMEHOW
+			
+			collection[i].style.transition = "all " + timer;
 			collection[i].style.opacity = 0;
 			
 		}
@@ -33,10 +33,34 @@
 		return this;
 	};
 	
-	
-	
-	
-	
-	/*Shift.prototype.fadeIn = function(){
+	shift.fn.fadeIn = function(_duration){
 		
-	};*/
+		var i, j, timer, callback, collection;
+		
+		collection = this.set;
+		timer = (_duration && typeof _duration === "number") ? _duration + "s" : "0.5s";
+		
+		for (i = 0; i < collection.length; i++){
+			
+			collection[i].style.visibility = "visible";
+			collection[i].style.transition = "all " + timer;
+			collection[i].style.opacity = 1;
+			
+		}
+		
+		callback = function(){
+			
+			// Reset all transitions after completion
+			//
+			for (j = 0; j < collection.length; j++){
+				collection[j].style.transition = "";
+			}
+			
+			collection[collection.length - 1].removeEventListener("transitionend", callback);
+		}
+		
+		collection[collection.length - 1].addEventListener("transitionend", callback);
+		
+		return this;
+		
+	};
