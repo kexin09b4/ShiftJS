859416f807582af2e500696c676f52a63a3bd93a
Dan Zervoudakes <dan.zervoudakes@gmail.com>
2016-05-15 10:07:11 -0600

    Update Gruntfile/package

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,45 +2,56 @@ module.exports = function(grunt) {
 
 	grunt.initConfig({
 		
-		pkg: grunt.file.readJSON("package.json"),
+		pkg: grunt.file.readJSON('package.json'),
+		
+		clean: {
+			build: {
+				src: ['dist']
+			}
+		},
 		
 		concat: {
 		    dist: {
 			    src: [
-				    "src/shift.js", // INCLUDE THIS FILE BEFORE ALL OTHERS
-		    		"src/animate.js",
-		    		"src/delay.js",
-		    		"src/fade.js",
-		    		"src/origin.js",
-		    		"src/rotate.js",
-		    		"src/scale.js",
-		    		"src/set.js",
-		    		"src/skew.js",
-		    		"src/translate.js"
+				    'src/shift.js', // UPDATE NOMENCLATURE AND DIRECTORIES, TEMPLATES, ETC. SIMILAR TO GRINDSTONE
+		    		'src/animate.js',
+		    		'src/delay.js',
+		    		'src/fade.js',
+		    		'src/origin.js',
+		    		'src/rotate.js',
+		    		'src/scale.js',
+		    		'src/set.js',
+		    		'src/skew.js',
+		    		'src/translate.js'
 			    ],
-			    dest: "dist/shift-v<%= pkg.version %>.js"
+			    dest: 'dist/shift-v<%= pkg.version %>.js'
 		    }
 		},
 		
 		uglify: {
 		    options: {
-		    	banner: "/* ShiftJS v<%= pkg.version %> | Copyright (c) <%= grunt.template.today('yyyy') %> Dan Zervoudakes | https://github.com/dzervoudakes/ShiftJS/blob/master/LICENSE */\n"
+		    	banner: '/* ShiftJS v<%= pkg.version %> | Copyright (c) <%= grunt.template.today('yyyy') %> Dan Zervoudakes | https://github.com/dzervoudakes/ShiftJS/blob/master/LICENSE */\n'
 		    },
 		    build: {
-            	src: "dist/shift-v<%= pkg.version %>.js",
-				dest: "dist/shift-v<%= pkg.version %>.min.js"
+            	src: 'dist/shift-v<%= pkg.version %>.js',
+				dest: 'dist/shift-v<%= pkg.version %>.min.js'
         	}
+		},
+		
+		watch: {
+			scripts: {
+				files: ['<%= concat.dist.src %>'],
+				tasks: ['build']
+			}
 		}
 		
 	});
 	
-	/* ADD "CLEAN" TASK HERE */
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 	
-	/* ADD GRUNT WATCH TASK AND JUST HAVE THE TEST HTML FILE REFERENCE THE OUTPUT */
-	
-	grunt.loadNpmTasks("grunt-contrib-concat");
-	grunt.loadNpmTasks("grunt-contrib-uglify");
-	
-	grunt.registerTask("build", ["concat", "uglify"]);
+	grunt.registerTask('build', ['clean', 'concat', 'uglify']);
 	
 };
